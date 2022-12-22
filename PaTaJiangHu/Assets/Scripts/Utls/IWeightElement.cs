8973badf3be636c2981be46c1ae4177c1e3f1b05
Leo <icefoxz@msn.com>
2022-12-22 14:31:28 +0800

    框架: 修正权重, 调整打乱逻辑

--- PaTaJiangHu/Assets/Scripts/Utls/IWeightElement.cs
+++ PaTaJiangHu/Assets/Scripts/Utls/IWeightElement.cs
@@ -11,25 +11,40 @@ namespace Utls
     public static class WeightElementExtension
     {
         /// <summary>
-        /// 权重计算。如果权重值>0将随机，如果0将会是保底值
+        /// 权重计算。如果权重值>0将随机，如果0将会是保底值(不会参与权重)
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="list"></param>
         /// <returns></returns>
         public static T WeightPick<T>(this IEnumerable<T> list) where T : IWeightElement =>
-            SortWeightElements(list).FirstOrDefault();
+            list.WeightTake(1).FirstOrDefault();
+
         /// <summary>
         /// 权重计算。如果权重值>0将随机，如果0将会是保底值
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="list"></param>
+        /// <param name="take"></param>
         /// <returns></returns>
-        public static IEnumerable<T> WeightTake<T>(this IEnumerable<T> list, int take) where T : IWeightElement =>
-            SortWeightElements(list).Take(take);
+        public static IEnumerable<T> WeightTake<T>(this IEnumerable<T> list, int take) where T : IWeightElement
+        {
+            var sortedList = SortWeightElements(list);
+            var tookList = sortedList.Take(take).ToArray();
+            var lastElement = tookList.Last();
+            var resolveList = tookList.Concat(sortedList.Except(tookList).Where(s=>s.Weight == lastElement.Weight)).ToArray();
+            if (resolveList.Length >= take)
+            {
+                //避免相同权重的元素仅获取上面的,所以再次打乱;
+                return resolveList.OrderByDescending(_ => Sys.Random.Next(0, 100)).Take(take); 
+            }
+            return resolveList;
+        }
 
         private static T[] SortWeightElements<T>(IEnumerable<T> list) where T : IWeightElement
         {
-            return list.Select(ResolveWithZero).OrderByDescending(w => w.weight).Select(s => s.obj).ToArray();
+            return list.Select(ResolveWithZero)
+                .OrderByDescending(w => w.weight)
+                .Select(s => s.obj).ToArray();
             //var ss = list.Select(ResolveWithZero).OrderByDescending(w => w.weight).ToArray();
             //var rr = ss.Select(s => s.obj).ToArray();
             //return rr;
