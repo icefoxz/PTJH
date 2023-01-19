b054b04907d968f201773db930d14d10363346ff
Tsurukage <tsunayoshihibari@gmail.com>
2023-01-19 20:15:59 +0800

    更新：弟子内伤+外伤

--- PaTaJiangHu/HotFix/Src/Managers/DiziAdvManager.cs
+++ PaTaJiangHu/HotFix/Src/Managers/DiziAdvManager.cs
@@ -121,8 +121,8 @@ public class DiziAdvManager
             ElementMgr.SetConValue(Conditions.Food, dizi.Food.Value, dizi.Food.Max);
             ElementMgr.SetConValue(Conditions.State, dizi.Emotion.Value, dizi.Emotion.Max);
             ElementMgr.SetConValue(Conditions.Silver, dizi.Silver.Value, dizi.Silver.Max);
-           //ElementMgr.SetConValue(Conditions.Injury, dizi.Injury.Value, dizi.Injury.Max);
-            //ElementMgr.SetConValue(Conditions.Inner, dizi.Inner.Value, dizi.Inner.Max);
+            ElementMgr.SetConValue(Conditions.Injury, dizi.Injury.Value, dizi.Injury.Max);
+            ElementMgr.SetConValue(Conditions.Inner, dizi.Inner.Value, dizi.Inner.Max);
         }
 
         private class Element_skill : UiBase
