e717b11f15be95b9d9bea9fdc102e5aab7324fd2
Lumina <tsunayoshihibari@gmail.com>
2023-03-03 19:50:16 +0800

    添加：Demo_View_Faction_InfoMgr.cs

--- PaTaJiangHu/HotFix/Src/Managers/GameScene/Demo_v1Agent.cs
+++ PaTaJiangHu/HotFix/Src/Managers/GameScene/Demo_v1Agent.cs
@@ -1,5 +1,6 @@
 ﻿
 using HotFix_Project.Managers.Demo_v1;
+using HotFix_Project.Src.Managers.Demo_v1;
 
 namespace HotFix_Project.Managers.GameScene;
 
@@ -7,8 +8,10 @@ namespace HotFix_Project.Managers.GameScene;
 internal class Demo_v1Agent : MainUiAgent
 {
     private Demo_View_PagesMgr Demo_View_PageMgr { get; }
+    private Demo_View_Faction_InfoMgr Demo_View_Faction_InfoMgr { get; }
     internal Demo_v1Agent(IMainUi mainUi) : base(mainUi)
     {
         Demo_View_PageMgr = new Demo_View_PagesMgr(this);
+        Demo_View_Faction_InfoMgr = new Demo_View_Faction_InfoMgr(this);
     }
 }
